e58b3409f432961c17f18cd25f85093c36b14bf8
yo <yo@luciditv.com>
2024-10-31 19:53:15 -0300

    test github pages_2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ssr } from 'vite-plugin-ssr/plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ssr({ prerender: true })],
   base: '/little-tools/',
 })
